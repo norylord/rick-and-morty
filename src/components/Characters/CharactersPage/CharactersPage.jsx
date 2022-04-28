29ca61fe5c71d34470d458fb8f22c69cbc6850d0
nrylord <nrylord@gmail.com>
2022-04-28 23:14:56 +0300

    CharacterPage Checkbox added

--- src/components/Characters/CharactersPage/CharactersPage.jsx
+++ src/components/Characters/CharactersPage/CharactersPage.jsx
@@ -10,8 +10,26 @@ const CharactersPage = () => {
         RequestService.allCharacters("character").then(res => setData(res))
     },[])
 
+    const changeParams = (type) => {
+        switch (type){
+            case 'alive':
+                setParams({...params, status: params.status === 'Alive' ? null : 'Alive'})
+                break
+        }
+    }
+
     return (
+<<<<<<< Updated upstream
         <div>
+=======
+        <div className='characterPage'>
+            <div className="searchContainer">
+                <Input placeholder="Enter Name" onChange={e => setParams({...params, name: e.target.value})}/>
+                <div className="checkboxContainer">
+                    <Checkbox name={'Alive'} id={"Alive"} value={'Alive'} label={'Alive'} onChange={() => changeParams('alive')}/>
+                </div>
+            </div>
+>>>>>>> Stashed changes
             <CharacterList data={data}/>
         </div>
     );
