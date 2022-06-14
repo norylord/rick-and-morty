de84dca6488af5bd6aef90cfaac8ef78bd496d50
nrylord <nrylord@gmail.com>
2022-06-14 22:56:29 +0300

    little changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import {Routes, Navigate, Route, BrowserRouter, HashRouter as Router} from 'react-router-dom'
 import "./styles/App.sass"
 import './styles/Normalize.css'
-import Home from "./pages/HomePage/HomePage"
 import AppLayout from "./layout/AppLayout/AppLayout";
 import EpisodePage from "./pages/EpisodesPage/EpisodePage";
 import CharacterInfo from "./components/Characters/CharacterInfo/CharacterInfo";
+import CharactersPage from "./pages/CharactersPage/CharactersPage";
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<AppLayout/>}>
-            <Route index element={<Home/>}/>
+            <Route index element={<CharactersPage/>}/>
             <Route path="episode" element={<EpisodePage/>}/>
             <Route path="character/:id" element={<CharacterInfo/>}/>
             <Route path="*" element={<Navigate to="/" replace/>}/>
