85419984d6eb2429b0b7f1061970358167b01609
aleksandrpletnev <norylord@icloud.com>
2026-07-27 01:03:50 +0300

    fix: убрать неиспользуемый код

--- src/entities/character/api/api.ts
+++ src/entities/character/api/api.ts
@@ -1,4 +1,4 @@
-import {api, ApiError, PaginatedResponse} from "@/shared/";
+import {api, PaginatedResponse} from "@/shared/";
 import {Character} from "../model/type";
 
 type FilterCharactersParams = Partial<{
@@ -15,15 +15,6 @@ type PaginationCharactersParams = {
 
 export type GetCharactersParams = FilterCharactersParams & PaginationCharactersParams
 
-const EMPTY_RESPONSE: PaginatedResponse<Character[]> = {
-	info: {count: 0, pages: 0, next: null, prev: null},
-	results: [],
-}
-
 export const getCharacters = async (params: GetCharactersParams): Promise<PaginatedResponse<Character[]>> => {
-	try {
-		return await api.get<PaginatedResponse<Character[]>>('/character', {params})
-	} catch (e) {
-		throw e
-	}
+	return api.get<PaginatedResponse<Character[]>>('/character', {params})
 }
--- src/shared/ui/PaginationControlled.tsx
+++ src/shared/ui/PaginationControlled.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {Pagination} from "@heroui/react";
-import {useState} from "react";
 
 type TProps = {
 	page: number,
