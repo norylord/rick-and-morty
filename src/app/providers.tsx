00a203be360aa84503e124806f7fdb7446a6cb91
aleksandrpletnev <norylord@icloud.com>
2026-07-26 23:59:40 +0300

    feat: провайдер TanStack Query

---
+++ src/app/providers.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import React from 'react';
+import {environmentManager, QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+
+declare global {
+	interface Window {
+		__TANSTACK_QUERY_CLIENT__?: QueryClient
+	}
+}
+
+let browserQueryClient: QueryClient | undefined
+const makeQueryClient = () => {
+	return new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 60 * 1000,
+				retry: false
+			}
+		}
+	})
+}
+
+function getQueryClient() {
+	if (environmentManager.isServer()) {
+		return makeQueryClient() // на сервере — всегда новый клиент (иначе утечёт кэш между запросами)
+	}
+	browserQueryClient ??= makeQueryClient()
+	return browserQueryClient
+}
+
+type TProps = {
+	children: React.ReactNode
+}
+
+const Providers = ({children}: TProps) => {
+	const queryClient = getQueryClient()
+
+	// Только для браузерного расширения TanStack Query.
+	// useEffect не выполняется на сервере, поэтому window здесь всегда есть.
+	React.useEffect(() => {
+		window.__TANSTACK_QUERY_CLIENT__ = queryClient
+	}, [queryClient])
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false}/>
+			{children}
+		</QueryClientProvider>
+	);
+};
+
+export default Providers;
