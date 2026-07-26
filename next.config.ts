2020f6fd62601941d75ebd0b6dc79dadbd708d67
aleksandrpletnev <norylord@icloud.com>
2026-07-26 23:59:28 +0300

    chore: обновить конфиги и зависимости

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+
+	images: {
+		remotePatterns: [
+			new URL('https://rickandmortyapi.com/api/**')
+		]
+	}
 };
 
 export default nextConfig;
