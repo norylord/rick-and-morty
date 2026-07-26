500e44ba68b30e1c0aa474a08146755ba4a5ed50
aleksandrpletnev <norylord@icloud.com>
2026-07-26 23:59:34 +0300

    feat: шапка приложения

---
+++ src/widgets/AppHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppHeader = () => {
+	return (
+		<header className={"flex justify-center items-center px-15 py-4"}>
+			<p className={"text-2xl uppercase"}>
+				Rick and Morty
+			</p>
+		</header>
+	);
+};
+
+export default AppHeader;
