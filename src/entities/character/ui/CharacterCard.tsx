bcfce0d6013f168cd4efb5c6e6553f50798fa866
aleksandrpletnev <norylord@icloud.com>
2026-07-28 03:10:12 +0300

    feat: новый вид карточки персонажа

--- src/entities/character/ui/CharacterCard.tsx
+++ src/entities/character/ui/CharacterCard.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import React from 'react';
 import {Character} from "@/entities/character";
 import Image from "next/image";
 import Link from "next/link";
+import {Button, Card} from "@heroui/react";
+import clsx from "clsx";
+import {useRouter} from "next/navigation";
 
 type TProps = {
 	character: Character
@@ -18,18 +23,42 @@ export const CharacterCardSkeleton = () => {
 }
 
 const CharacterCard = (props: TProps) => {
+
+	const router = useRouter()
+	const aliveColor = props.character.status === 'Alive' ? '#00ff00' : props.character.status === 'Dead' ? '#9a0000' : '#037994'
+
 	return (
-		<Link href={`/characters/${props.character.id}`}
-		      className={[cardClass, "h-full w-full border border-gray-500/20 bg-mist-800 grid grid-cols-2"].join(' ')}>
-			<div className={' p-4'}>
-				<p className={"text-xl"}>
-					{props.character.name}
-				</p>
-			</div>
-			<div>
+
+		<Link href={`/characters/${props.character.id}`}>
+			<Card className={'size-full relative overflow-hidden group aspect-video text-foreground'} variant={'transparent'}>
 				<Image src={props.character.image} alt={props.character.name} width={300} height={300}
-				       className={'size-full object-cover'}/>
-			</div>
+				       className={'rounded-xl absolute inset-0 size-full object-cover -z-1 dark:brightness-50 dark:blur-xs blur-md group-hover:blur-[0px] transition-all duration-300 scale-[1.2] group-hover:scale-100'}/>
+				<Card.Header>
+					<Card.Title className={'text-xl font-semibold text-foreground'}>
+						{props.character.name}
+					</Card.Title>
+				</Card.Header>
+
+				<Card.Content className={'bg-transparent'}>
+				</Card.Content>
+				<Card.Footer>
+					<div className={'space-x-2'}>
+						<p className={'font-semibold max-w-60 overflow-hidden text-ellipsis line-clamp-1'}>
+							{props.character.location.name}
+						</p>
+						<div className={'flex items-center gap-2'}>
+							<div className={clsx('size-2 rounded-full')} style={{background: aliveColor}}/>
+							<p className={'opacity-55'}>
+								{props.character.status}
+							</p>
+						</div>
+					</div>
+					<Button variant={'ghost'} className={'ml-auto'}
+					        onClick={() => router.push(`/characters/${props.character.id}`)}>
+						Подробнее
+					</Button>
+				</Card.Footer>
+			</Card>
 		</Link>
 	);
 };
